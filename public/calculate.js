471d4ff1485fdd1404ea866de4491c6f02993e8d
jnprogrammer <joshuanparker@gmail.com>
2017-09-22 23:05:12 -0400

    Writing boiler plate

---
+++ public/calculate.js
@@ -0,0 +1,24 @@
+
+
+var runningTotal, leftNum, rightNum;
+
+
+function Addition(num1, num2)
+{
+    return num1 + num2;
+}
+
+function Subtraction(num1, num2)
+{
+    return num1 - num2;
+}
+
+function Division(num1 , num2)
+{
+    return num1 / num2;
+}
+
+function Multiplication(num1 , num2)
+{
+    return num1 * num2;
+}
