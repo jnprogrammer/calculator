ef4e7d72fdddebf015f99a0d1e9adcf2f827f6f9
jnprogrammer <joshuanparker@gmail.com>
2017-11-06 22:15:56 -0500

    Started UI design

--- public/calculate.js
+++ public/calculate.js
@@ -31,3 +31,15 @@ function calculate(num1, num2)
 function calculatorBox() {
   //  document.getElementById('inputBox').value=''
 } 
+
+function add_field()
+{
+  var total_text=document.getElementsByClassName("input_text");
+  total_text=total_text.length+1;
+  document.getElementById("field_div").innerHTML=document.getElementById("field_div").innerHTML+
+  "<p id='input_text"+total_text+"_wrapper'><input type='text' class='input_text' id='input_text"+total_text+"' placeholder='Enter Text'><input type='button' value='Remove' onclick=remove_field('input_text"+total_text+"');></p>";
+}
+function remove_field(id)
+{
+  document.getElementById(id+"_wrapper").innerHTML="";
+}
