81385214d01a77756a175f52c66d246b0ca55d75
jnprogrammer <joshuanparker@gmail.com>
2017-11-09 17:50:54 -0500

    updated js code for calculator

--- public/calculate.js
+++ public/calculate.js
@@ -4,5 +4,23 @@ $(document).ready(function(){
     //String to store the current inputstring
     var totalString;
     var operators1 = ["+","-","/","*"];
-    
+    var operators2 = ["."];
+    var nums=[0,1,2,3,4,5,6,7,8,9]
+
+    function getVal(input){
+
+    }
+    function update(){
+        totalString = input.join("");
+        $("#steps").html(totalString);
+    }
+    function getTotal(){
+       totalString = input.join("");
+       $("#steps").html(eval(totalString));
+    }
+    $("a").on("click",function(){
+        if(this.id==="deleteAll"){
+            
+        }
+    });
 });
